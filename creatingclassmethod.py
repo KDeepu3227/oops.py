eb892ae9a87e57f8bced670bc0eba9fcf07ed355
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-25 11:42:14 +0530

    Create creatingclassmethod.py

---
+++ creatingclassmethod.py
@@ -0,0 +1,12 @@
+###
+create a method with 2 parameters name,location,and pass the parameter?
+###
+class Details:
+    def d1(self,name,location):
+        print('name=',name)
+        print('location=',location)
+d = Details()
+d.d1('deepika','chowdepalli')
+###
+name= deepika
+location= chowdepalli
