31bbf873de8f73cf43495dab920b016c91dfa75d
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-25 12:03:16 +0530

    Create classnamesasoperators.py

---
+++ classnamesasoperators.py
@@ -0,0 +1,15 @@
+###
+create a class named operators define a functiomn named as sum which take 2 parameters and the function should return sum of call that method?
+###
+x = int(input("enter the number:"))
+y = int(input("enter the number:"))
+class Operators:
+    def sum(self,x,y):
+        return x+y
+s = Operators()
+result = s.sum(x,y)
+print(result)
+###
+enter the number:38
+enter the number:36
+74
